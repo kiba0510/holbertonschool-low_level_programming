918ab775de6e44b0a3e2d7680a86aae78ef54076
kiba0510 <kiba0510@gmail.com>
2020-09-16 22:50:11 +0000

    added new C program 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char charType;
+int intType;
+long int longType;
+long long int longlongType;
+float floatType;
+
+/*sizeof evaluates the size of a variable*/
+printf("Size of char: %ld bytes\n", sizeof(charType));
+printf("Size of int: %ld bytes\n", sizeof(intType));
+printf("Size of long int: %li bytes\n", sizeof(longType));
+printf("Size of long long int: %ld bytes\n", sizeof(longlongType));
+printf("Size of float: %ld\n", sizeof(floatType));
+return (0);
+}
