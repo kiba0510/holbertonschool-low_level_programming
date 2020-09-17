6dba8f0432223d08c89e136f588c8fe8d889c6db
kiba0510 <kiba0510@gmail.com>
2020-09-17 21:37:47 +0000

    added a new C file 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ if (x == 0)
 {
 printf("Last digit of %d is %d and is 0\n", n, x);
 }
-if (x < 0 && x != 0)
+if (x < 6 && x != 0)
 {
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x);
 }
