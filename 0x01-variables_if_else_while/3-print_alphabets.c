eef69dc2b4fef4528195f7d3e719775d2390b5b0
kiba0510 <kiba0510@gmail.com>
2020-09-17 22:34:10 +0000

    added new C file 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char x, y;
+for (x = 'a'; x <= 'z'; x++)
+{
+putchar(x);
+}
+for (y = 'A'; y <= 'Z'; y++)
+{
+putchar(y);
+}
+{
+putchar('\n');
+}
+return (0);
+}
