6f084004918c533bdd0d6a4d2a6b6f5e7703539b
kiba0510 <kiba0510@gmail.com>
2020-09-17 21:32:28 +0000

    added a new C file 1-last_digit.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+for (int x = 'A'; x <= 'Z'; x++)
+{
+putchar(lower_x));
+}
+return (0);
+}
