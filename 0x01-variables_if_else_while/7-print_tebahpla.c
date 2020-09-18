21d577d5fa41389995a7c3478271e74530c585ef
kiba0510 <kiba0510@gmail.com>
2020-09-18 01:10:56 +0000

    added new C file 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -6,13 +6,13 @@
  */
 int main(void)
 {
-	char x;
-	for (x = 'z'; x >= 'a'; x--)
-	{
-		putchar(x);
-	}
-	{
-		putchar('\n');
-	}
-	return (0);
+char x;
+for (x = 'z'; x >= 'a'; x--)
+{
+putchar(x);
+}
+{
+putchar('\n');
+}
+return (0);
 }
