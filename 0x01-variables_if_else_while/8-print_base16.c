99ccc4f7866cfc48a4fac1919d74a2665d612456
kiba0510 <kiba0510@gmail.com>
2020-09-18 01:25:36 +0000

    added new C 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char x, y;
+for (x = '0'; x <= '9'; x++)
+{
+putchar(x);
+}
+for (y = 'a'; y <= 'f'; y++)
+{
+putchar(y);
+}
+{
+putchar('\n');
+}
+return (0);
+}
