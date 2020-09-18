a812897254f1c33b1accb7c4de5d532243981f65
kiba0510 <kiba0510@gmail.com>
2020-09-18 03:42:53 +0000

    added new C file 10-print_comb2.c

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Writes 00 - 99
+ * @void: Empty parameter list for main.
+ *
+ * Description: Writes number 00-99
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+int i, j;
+
+for (i = 48; i <= 57; i++)
+{
+for (j = 48; j <= 57; j++)
+{
+putchar(i);
+putchar(j);
+if (i + j < 114)
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+putchar(10);
+return (0);
+}
