cce859741c2af99ef065e2f0eb1a01be57aec30b
kiba0510 <kiba0510@gmail.com>
2020-09-22 19:29:40 +0000

    added new C file 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ *print_sign - check if n is a sign
+ *@n: is entry point
+ *Return: 0
+ **/
+int print_sign(int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
