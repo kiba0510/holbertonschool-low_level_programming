4f7aa44d47f51276337a692b2649ff028b87992c
kiba0510 <kiba0510@gmail.com>
2020-09-24 21:12:33 +0000

    added new C file 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ *print_line - print the value of n 
+ *@a: entry point
+ *
+ */
+void print_line(int n)
+{
+	int a;
+
+	if (n > 0)
+	{
+	for (a = 0; a < n ; a++)
+	{
+        _putchar('_');
+	}
+	_putchar('\n');
+	}
+	else
+	{
+	_putchar('\n');
+	}
+}
