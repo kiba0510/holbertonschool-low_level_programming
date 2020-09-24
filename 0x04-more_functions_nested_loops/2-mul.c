3e2b5423a6957bd2ff22a94cfe0cf7b3298b9ce6
kiba0510 <kiba0510@gmail.com>
2020-09-24 16:46:32 +0000

    added new C file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+
+/**
+ * mul - time two ints
+ * @a: first int to mul
+ * @b: second int to mul
+ * Return: multiplication of both ints
+ **/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
