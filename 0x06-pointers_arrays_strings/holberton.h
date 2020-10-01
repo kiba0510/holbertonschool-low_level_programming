8b4da53b891070a3455ee332fd6a89e39639b666
kiba0510 <kiba0510@gmail.com>
2020-10-01 23:36:39 +0000

    added new C file 2-strncpy.c

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -32,3 +32,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
