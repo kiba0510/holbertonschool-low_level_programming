8a290737ad754c9f692b326a6a36ebea160aba4d
kiba0510 <kiba0510@gmail.com>
2020-10-05 21:14:14 +0000

    added new C file 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ *_strspn - get length of a prefix substring
+ *@s: string
+ *@accept: bytes being compared 
+ *Return: bytes to get to accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, exit;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		exit = 0;
+		for (j = 0; accept[j] != '\0'; j++)
+			if (accept[j] == s[i])
+			{
+				exit = 1;
+			}
+
+			if (exit == 0)
+			{
+				return (i);
+			}
+	}
+	return (i);
+}
