6f80775745062a753aef421a1021bff99b45350f
kiba0510 <kiba0510@gmail.com>
2020-10-05 19:22:41 +0000

    added new C file 2-strchr.c and updated holberton.h file

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ *_strchr - locates a character in a string
+ *@s: pointer to the string
+ *@c: character to search for
+ *Return: pointer to char
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+	}
+
+	if (s[i] == c)
+	{
+		return (&s[i]);
+	}
+
+	return (s);
+}
