5536e779aaf4baf160d90455131c7be88bcff0f4
kiba0510 <kiba0510@gmail.com>
2020-10-07 21:56:19 +0000

    added new C file 2-strlen_recursion.c holberton.h

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ *_strlen_recursion - function that returns the length of a string
+ *@s: string 
+ *Return: string length
+ */
+int _strlen_recursion(char *s)
+{
+	if (s[0] == '\0')
+		return (0);
+
+	if (s[1] == '\0')
+	{
+		return (1);
+	}
+	else
+	{
+		return (_strlen_recursion(&s[1]) + 1);
+	}			
+}
