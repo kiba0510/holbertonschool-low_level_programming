8166acd671b0137f48675254457624d9570e2d84
kiba0510 <kiba0510@gmail.com>
2020-10-07 22:46:19 +0000

    added new C file 3-factorial.c holberton.h

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *factorial - returns the factorial of a given number
+ *@n: number
+ *Return: factorial of n
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		n = factorial(n - 1) * n;
+		return (n);
+	}
+}
