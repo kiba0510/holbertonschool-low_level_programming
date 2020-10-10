3831ed2d3111b0e49039eb0a4c9c4605d960c4ff
kiba0510 <kiba0510@gmail.com>
2020-10-10 16:26:42 +0000

    added new readme file and C file 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - prints name of the file
+ *@argc: argument count
+ *@argv: argument vector
+ *Return: zero
+ */
+int main (int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	argc = argc;
+
+	return (0);
+}
