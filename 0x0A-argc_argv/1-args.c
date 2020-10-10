7510bdfa36eea0ad93a4db95bc667313539234b0
kiba0510 <kiba0510@gmail.com>
2020-10-10 16:42:34 +0000

    added a new C file 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ *main - prints the number of arguments
+ *@argc: argument count
+ *@argv: argument vector
+ *Return: zero
+ */
+int main (int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	argv = argv;
+
+	return (0);
+}
