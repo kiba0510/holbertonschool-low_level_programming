bc6f057b5079978cf8065103dcb365888ad9f00d
kiba0510 <kiba0510@gmail.com>
2020-10-15 22:51:51 +0000

    added new C file 2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *_memset - copies a given char 
+ *@s: given string to be copied
+ *@b: input
+ *@n: number of bytes
+ *Return: string
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
+
+/**
+ *_calloc - allocates memory for array using malloc
+ *@nmemb: number of elements
+ *@size: size in bytes
+ *Return: pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *p;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	p = malloc(nmemb * size);
+
+	if (p == NULL)
+		return (NULL);
+
+	_memset(p, 0, (nmemb * size));
+	return (p);
+}
