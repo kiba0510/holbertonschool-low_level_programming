f6c1d088269d60e0193ee38963c4cf77d83e58bf
kiba0510 <kiba0510@gmail.com>
2020-10-15 21:10:36 +0000

    HOTFIX6 to C file 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,53 +3,51 @@
 #include <stdlib.h>
 
 /**
- *_strlen - gives the length of a given string
- *@s: string to count
- *Return: length of a given string as integer
+ * string_nconcat - concatenates two strings.
+ * @s1: first string
+ * @s2: second string
+ * @n: index
+ * Return: char pointer
  */
-int _strlen(char *s)
-{
-	unsigned int i;
 
-	if (s == NULL)
-		return (1);
-
-	for (i = 0; s[i] != '\0'; i++)
-		;
-	return (i + 1);
-}
-
-/**
- *string_nconcat - concatenate n number of chars of s2 string to s1 string
- *@s1: first string
- *@s2: second string
- *@n: number of chars to concatenate
- *Return: pointer to string, NULL if fails
- */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *dest;
-	unsigned int size1, size2, i, j;
+	char *p;
+	unsigned int size1 = 0, size2 = 0, i;
 
-	size1 = _strlen(s1);
-	size2 = _strlen(s2);
-	if (n > size2 - 1)
-		n = size2;
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
 
-	dest = (char *)malloc(((size1 + n) + 1) * sizeof(char));
-	
-	for (i = 0; i < size1 - 1; i++)
-		dest[i] = s1[i];
-
-	for (j = 0; j <= n; j++)
+	while (s1[size1] != '\0')
 	{
-		dest[i + j] = s2[j];
+		size1++;
 	}
-	dest[i + j] = '\0';
 
-	return (dest);
+	while (s2[size2] != '\0')
+	{
+		size2++;
+	}
+
+	if (n > size2)
+		n = size2;
+	p = malloc((size1 + n + 1) * sizeof(char));
+
+	if (p == NULL)
+		return (0);
+
+	for (i = 0; i < size1; i++)
+	{
+		p[i] = s1[i];
+	}
+
+	for (; i < (size1 + n); i++)
+	{
+		p[i] = s2[i - size1];
+	}
+	p[i] = '\0';
+
+	return (p);
 }
