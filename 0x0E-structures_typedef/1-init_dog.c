7099ec751d0124d4e2e4bd1f3a493af1613f78c2
kiba0510 <kiba0510@gmail.com>
2020-10-19 20:21:19 +0000

    added new C file 1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ *init dog - initialize the variable struct dog
+ *@d: pointer to struct dog
+ *@name: pointer to pointer for dog´s name
+ *@age: variable for dog´s age
+ *@owner: pointer to variable for owner´s name
+ *Return: zero
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
