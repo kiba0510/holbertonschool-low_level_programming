7dc44e17739ed1e1db4329d4046f63d0fb4bdf8c
kiba0510 <kiba0510@gmail.com>
2020-10-24 22:24:00 +0000

    added new H file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,5 +7,21 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void print_c(va_list s);
+void print_i(va_list s);
+void print_f(va_list s);
+void print_s(va_list s);
+
+/**
+ *struct frt - structure format
+ *@f: format type
+ *@p: print function
+ */
+
+typedef struct frt
+{
+	char *f;
+	void (*p)(va_list s);
+}my_frt;
 
 #endif
