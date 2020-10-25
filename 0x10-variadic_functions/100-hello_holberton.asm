ea4e039aaec92660e1540d8dbfd727faa2e7bd57
kiba0510 <kiba0510@gmail.com>
2020-10-25 16:50:10 +0000

    added new asm file

---
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -0,0 +1,14 @@
+global main
+	section .text
+	
+main:	mov 	rax, 1
+	mov 	rdi, 1
+	mov	rsi, msg
+	mov	rdx, 17
+	syscall
+	mov	rax, 60
+	mov	rdi, 0
+	syscall
+
+	section .data
+	msg db "Hello, Holberton", 10
