986edd3ff9b68eaa4ea11d4b184ea55d2255af9c
kiba0510 <kiba0510@gmail.com>
2020-10-31 17:06:23 +0000

    added new c file

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ *free_list - Frees a list of nodes
+ *@head: Pointer to the first element of the list
+ */
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	if (head == NULL)
+		return;
+	while (head->next != NULL)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
+	free(head->str);
+	free(head);
+}
