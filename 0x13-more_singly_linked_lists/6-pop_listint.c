ea0399fbc393e2bc44c4d84107d2be9ea8ae1e1f
kiba0510 <kiba0510@gmail.com>
2020-11-04 20:31:05 +0000

    6-pop_listint.c file added

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ *int poplistin - Deletes the first element of a list
+ *@head: Pointer to the head of the list
+ *Return: The value of the deleted node
+ */
+
+int pop_listint(listint_t **head)
+{
+	int n;
+	listint_t *tmp;
+
+	if (head == NULL || *head == NULL)
+		return (0);
+	n = (*head)->n;
+	tmp = *head;
+	*head = (*head)->next;
+	free(tmp);
+	return (n);
+}
