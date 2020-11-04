fa9b8c87f57bf273fec2deedc8f4f55a1923bda6
kiba0510 <kiba0510@gmail.com>
2020-11-04 17:34:02 +0000

    added new c file

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ *free_listint - Frees a list of nodes
+ *@head: Pointer to the first node of the list
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	if (head == NULL)
+		return;
+
+	while (head->next != NULL)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+	free(head);
+}
