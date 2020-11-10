a8fb1c2f5227c546d99f84cdb807bc7abf3324d8
Julian Archbold <70171411+kiba0510@users.noreply.github.com>
2020-11-09 23:06:57 -0500

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,31 +1,46 @@
 #include "holberton.h"
 /**
- * append_text_to_file - Appends text at the end of a file.
- * @filename: Name of the file.
- * @text_content: String to append at the end of a file.
- * Return: 1 if the file exist, -1 if the file does not exist, or if you
- * don't have the required permissions to write to the file.
- */
-int append_text_to_file(const char *filename, char *text_content)
+  * _strlen - length of a string
+  * @s: input char
+  * Return: length of a string
+**/
+int _strlen(char *s)
 {
-	int fd;
-	int i;
+	int i = 0;
 
-	if (filename == NULL)
-		return (-1);
-	if (text_content == NULL)
-		return (1);
-	fd = open(filename, O_APPEND | O_WRONLY);
-	if (fd == -1)
-		return (-1);
-	for (i = 0; text_content[i] != '\0'; i++)
+	while (s[i])
 	{
-		if (write(fd, &text_content[i], 1) == -1)
+		i++;
+	}
+	return (i);
+}
+/**
+* create_file - check the code for Holberton School students.
+* @filename: file to create.
+* @text_content: info to write into the file.
+* Return: 1 on success, -1 on failure
+*/
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t nletters;
+	int file;
+
+	if (!filename)
+		return (1);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (file == -1)
+	{
+		return (-1);
+	}
+	if (text_content)
+	{
+		nletters = write(file, text_content, _strlen(text_content));
+		if (nletters == -1)
 		{
-			close(fd);
+			close(file);
 			return (-1);
 		}
 	}
-	close(fd);
+	close(file);
 	return (1);
 }
