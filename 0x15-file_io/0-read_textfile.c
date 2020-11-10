f9a2b1086f393b62f7b4c7e52300ea29d3fcc275
Julian Archbold <70171411+kiba0510@users.noreply.github.com>
2020-11-09 23:05:11 -0500

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,49 +1,42 @@
 #include "holberton.h"
 /**
- * read_textfile - Reads a file and prints it to the POSIX stdout.
- * @filename: The name of the file that needs to be read.
- * @letters: Is the number of letters the function should print.
- * Return: The actual number of letters it could read and print.
- */
-
+* read_textfile - check the code for Holberton School students.
+* @filename: file to read and write
+* @letters: number of letters to read and write.
+* Return: letters printed
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	int i;
-	int read_letters;
-	char *buf;
+	ssize_t nletters;
+	int file;
+	char *text;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	if (buf == NULL)
+	text = malloc(sizeof(char) * letters + 1);
+	if (text == NULL)
 		return (0);
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	file = open(filename, O_RDONLY);
+	if (file == -1)
 	{
-		free(buf);
+		free(text);
 		return (0);
 	}
-
-	read_letters = read(fd, buf, letters);
-	if (read_letters == -1)
+	nletters = read(file, text, sizeof(char) * letters);
+	if (nletters == -1)
 	{
-		close(fd);
-		free(buf);
+		free(text);
+		close(file);
 		return (0);
 	}
-
-	for (i = 0; i < read_letters; i++)
+	nletters = write(STDOUT_FILENO, text, nletters);
+	if (nletters == -1)
 	{
-		if (write(STDOUT_FILENO, &buf[i], 1) == -1)
-		{
-			close(fd);
-			free(buf);
-			return (0);
-		}
-
+		free(text);
+		close(file);
+		return (0);
 	}
-	close(fd);
-	free(buf);
-	return (read_letters);
+	free(text);
+	close(file);
+	return (nletters);
 }
