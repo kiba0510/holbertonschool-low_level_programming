e9c084798082a8c097ee1a65427ecd53de6fedfb
Julian Archbold <70171411+kiba0510@users.noreply.github.com>
2020-11-09 23:11:30 -0500

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,19 +15,19 @@ int _strlen(char *s)
 	return (i);
 }
 /**
-* create_file - check the code for Holberton School students.
-* @filename: file to create.
-* @text_content: info to write into the file.
+* append_text_to_file - appends text at the end of a file.
+* @filename: file to append.
+* @text_content: info to append into the file.
 * Return: 1 on success, -1 on failure
 */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	ssize_t nletters;
 	int file;
 
 	if (!filename)
-		return (1);
-	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+		return (-1);
+	file = open(filename, O_WRONLY | O_APPEND);
 	if (file == -1)
 	{
 		return (-1);
