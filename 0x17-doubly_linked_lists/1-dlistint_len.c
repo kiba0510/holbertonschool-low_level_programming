46cd47da2e21dc4dbbb71538553c3c0373cd7a37
kiba0510 <kiba0510@gmail.com>
2020-12-14 17:29:50 +0000

    Function that returns number of elements in a list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ *dlistint_len - Counts and return all the elements in a dlistint_t list
+ *@h: Pointer to the Head of a list
+ *Return: Number of nodes in a list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i;
+/**
+	if (h == NULL)
+		return (0);
+	for (i = 0; h != NULL; i++);
+	h = h->next;
+	return (i);
+*/
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
