35f993ef3f8c10db621401cc4d73956472b5e865
kiba0510 <kiba0510@gmail.com>
2021-04-21 01:40:47 +0000

    betty hotfix

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,16 +12,16 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-    size_t i = 0;
+	size_t i = 0;
 
-    if (!array)
-        return (-1);
-    while (i < size)
-    {
-        printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-            return (i);
-        i++;
-    }
-    return (-1);
+	if (!array)
+		return (-1);
+	while (i < size)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+		i++;
+	}
+	return (-1);
 }
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -11,34 +11,34 @@
  **/
 int binary_search(int *array, size_t size, int value)
 {
-    size_t left = 0, right = size - 1, i = 0;
-    int half;
+	size_t left = 0, right = size - 1, i = 0;
+	int half;
 
-    if (!array || !size)
-    {
-        return (-1);
-    }
+	if (!array || !size)
+	{
+		return (-1);
+	}
 
-    while (left <= right)
-    {
-        printf("Searching in array: ");
+	while (left <= right)
+	{
+		printf("Searching in array: ");
 
-        for (i = left; i < right; i++)
-        {
-            printf("%d, ", array[i]);
-        }
-        printf("%d\n", array[right]);
+		for (i = left; i < right; i++)
+		{
+			printf("%d, ", array[i]);
+		}
+		printf("%d\n", array[right]);
 
-        half = (right + left) / 2;
+		half = (right + left) / 2;
 
-        if (array[half] == value)
-            return (half);
+		if (array[half] == value)
+			return (half);
 
-        else if (array[half] > value)
-            right = half - 1;
+		else if (array[half] > value)
+			right = half - 1;
 
-        else
-            left = half + 1;
-    }
-    return (-1);
+		else
+			left = half + 1;
+	}
+	return (-1);
 }
